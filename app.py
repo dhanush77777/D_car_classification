580918d24fbb8c15cd2b5e7e3040edbb6692d1e9
ig_Dhanush <66821432+dhanush77777@users.noreply.github.com>
2021-03-26 12:57:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,10 +7,10 @@ Created on Thu Jun 11 22:34:20 2020
 
 from __future__ import division, print_function
 # coding=utf-8
-import sys
+
 import os
-import glob
-import re
+
+
 import numpy as np
 
 # Keras
